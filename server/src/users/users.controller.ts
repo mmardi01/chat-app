9125915e9ad23b6c7f58c2c24a718f1070d471bb
mmardi01 <mmardi@student.1337.ma>
2024-02-13 21:10:26 +0100

    add get user and all users

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from 'src/auth/Guards/AuthGuard';
 import { Request } from 'express';
@@ -10,6 +10,16 @@ export class UsersController {
 
   @Get()
   async getUser(@Req() req: Request) {
-    return this.userService.findUser(req); 
+    return this.userService.getUser(req['user'].id); 
+  }
+
+  @Get('profile')
+  async findUser(@Query('id') id: string) {
+    return this.userService.getUser(id);
+  }
+
+  @Get('all')
+  async findAll() {
+    return this.userService.getAll();
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
 import { Repository } from 'typeorm';
@@ -8,19 +8,35 @@ import { Request } from 'express';
 export class UsersService {
   constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
 
-  async findUser(req: Request) {
+  async getUser(id: string) {
     try {
       const user = this.userRepo.findOne({
         where: {
-          id: req['user'].id,
+          id: id
         },
       });
 
       delete (await user).password;
-
       return user;
     } catch (error) {
       throw new NotFoundException('user not found.');
+    }
+  }
+
+  async getAll() {
+    try {
+      const users = this.userRepo.find({
+        select: {
+          'id':true,
+          'username':true,
+          'email':true,
+          'image':true,
+        }
+      });
+      return users;
+    }
+    catch(e){
+      throw new ForbiddenException();
     }
   }
 }
